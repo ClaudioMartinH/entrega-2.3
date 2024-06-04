e010b202c28ad7c1ca50001831bde0a69d115943
ClaudioMartinH <160405164+ClaudioMartinH@users.noreply.github.com>
2024-06-05 00:45:47 +0200

    testing

--- testing/index.test.ts
+++ testing/index.test.ts
@@ -5,13 +5,13 @@ import { expect, beforeEach, describe, test } from "vitest"
 describe("memoize function" , () => {
   
   test("fibonacci fired without memoize function", () => {
-    
+    restartCount();
     expect(fibonacci(5)).toBe(5);
     expect(getCount()).toBe(15);
   })
    
   test("memoized fibonacci", () => {
-    
+    restartCount();
     expect(fib(5)).toBe(5);
     expect(getCount()).toBeLessThan(15);
   })
