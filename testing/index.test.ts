247a5e0cd1fdbb8d2543d2b59b0112934117a312
ClaudioMartinH <160405164+ClaudioMartinH@users.noreply.github.com>
2024-06-05 00:47:41 +0200

    tests en rojo en vsc en verde en actions

--- testing/index.test.ts
+++ testing/index.test.ts
@@ -3,15 +3,16 @@ import { expect, beforeEach, describe, test } from "vitest"
 
 
 describe("memoize function" , () => {
+  beforeEach(() => {
+    restartCount();
+  })
   
   test("fibonacci fired without memoize function", () => {
-    restartCount();
     expect(fibonacci(5)).toBe(5);
     expect(getCount()).toBe(15);
   })
    
   test("memoized fibonacci", () => {
-    restartCount();
     expect(fib(5)).toBe(5);
     expect(getCount()).toBeLessThan(15);
   })
